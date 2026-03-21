2bd5ae17cebff521c267ef6733560943e54a2e34
Feeeeddmmmeee <69256898+Feeeeddmmmeee@users.noreply.github.com>
2026-03-21 14:20:42 +0100

    added a shared vector basis

--- LynxEngine/include/LynxEngine/Core.h
+++ LynxEngine/include/LynxEngine/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include <memory>
 #include <utility>
 
@@ -13,4 +15,15 @@ namespace Lynx
 	{
 		return std::make_shared<T>(std::forward(args)...);
 	}
+
+	class Vec3
+	{
+		public:
+			static constexpr glm::vec3 UP = {0, 0, 1};
+			static constexpr glm::vec3 DOWN = {0, 0, -1};
+			static constexpr glm::vec3 FRONT = {0, 1, 0};
+			static constexpr glm::vec3 BACK = {0, -1, 0};
+			static constexpr glm::vec3 LEFT = {1, 0, 0};
+			static constexpr glm::vec3 RIGHT = {-1, 0, 0};
+	};
 }
