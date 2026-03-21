d9d1fd477a2ff2979a9b648588222defbe730a9a
Feeeeddmmmeee <69256898+Feeeeddmmmeee@users.noreply.github.com>
2026-03-21 14:54:35 +0100

    more libraries in the pch

--- LynxEngine/include/LynxPch.h
+++ LynxEngine/include/LynxPch.h
@@ -8,6 +8,17 @@
 #include <queue>
 #include <unordered_map>
 #include <map>
+#include <algorithm>
+#include <limits>
+#include <array>
+#include <chrono>
+
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_raii.hpp>
+
+#include <stb/stb_image.h>
+#include <tiny_obj_loader.h>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
@@ -15,3 +26,4 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/random.hpp>
+#include <glm/gtx/hash.hpp>
